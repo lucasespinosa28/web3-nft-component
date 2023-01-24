db724100c29d2d8be370ea7ce2691d5e0e3aa726
lucasespinosa28 <52639395+lucasespinosa28@users.noreply.github.com>
2023-01-24 20:56:35 +0000

    storybook

--- .storybook/main.cjs
+++ .storybook/main.cjs
@@ -1,18 +1,7 @@
 module.exports = {
-  "stories": [
-    "../src/**/*.stories.mdx",
-    "../src/**/*.stories.@(js|jsx|ts|tsx)"
-  ],
-  "addons": [
-    "@storybook/addon-links",
-    "@storybook/addon-essentials",
-    "@storybook/addon-interactions"
-  ],
-  "framework": "@storybook/react",
-  "core": {
-    "builder": "@storybook/builder-vite"
+  stories: ["../src/**/*.stories.mdx","../src/**/*.stories.@(js|jsx|ts|tsx)"],
+  addons: ['@storybook/addon-links', '@storybook/addon-essentials'],
+  core: {
+    builder: '@storybook/builder-vite', // 👈 The builder enabled here.
   },
-  "features": {
-    "storyStoreV7": true
-  }
-}
+};
---
+++ src/components/DisplayNFT.jsx
@@ -0,0 +1,5 @@
+import React from "react";
+
+export default function DisplayNFT(){
+    return (<h1>Hello NFT</h1>)
+}
